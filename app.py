53d4e3141ef39dd8a178716798805ae648392ed0
Sova98 <marina.95.m@mail.ru>
2018-12-11 00:38:49 +0300

    yy

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 import os
 from flask import Flask
 from flask import render_template
+from flask import Flask, request
+import telepot
 
 app = Flask(__name__)
 
-@app.route("/")
+secret = "bddad84a-f6a3-445b-8f1d-31f9e3b4697b"
+bot = telepot.Bot('658605557:AAGQrDKPH3dqtPbDjvEK_I9BcW4VSr-A5yk')
+bot.setWebhook("https://sovervo98.scalingo.io/{}".format(secret), max_connections=1)
+
+@app.route('/{}'.format(secret), methods=["POST"])
 def hello():
-    return render_template('index.html')
+    update = request.get_json()
+    print("updates", update)
+    if "message" in update:
+        global users
+        text = update["message"]["text"]
+        chat_id = update["message"]["chat"]["id"]
+        if chat_id not in users:
+            users.append(chat_id)
+        bot.sendMessage(chat_id, "From the web: you said '{}'".format(text))
+    return "OK"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
