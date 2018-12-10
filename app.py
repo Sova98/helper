412f0461cfb8ae1c5cdcdd0242858b5511218366
Sova98 <marina.95.m@mail.ru>
2018-12-10 23:30:30 +0300

    18th

--- app.py
+++ app.py
@@ -1,48 +1,24 @@
+import requests
 from flask import Flask, request
-import telepot
-import urllib3
-from threading import Timer
-import os
-"""proxy_url = "http://proxy.server:3128"
-telepot.api._pools = {
-    'default': urllib3.ProxyManager(proxy_url=proxy_url, num_pools=3, maxsize=10, retries=False, timeout=30),
-}
-telepot.api._onetime_pool_spec = (urllib3.ProxyManager, dict(proxy_url=proxy_url, num_pools=1, maxsize=1, retries=False, timeout=30))
-"""
-secret = "bddad84a-f6a3-445b-8f1d-31f9e3b4697b"
-bot = telepot.Bot('658605557:AAGQrDKPH3dqtPbDjvEK_I9BcW4VSr-A5yk')
-bot.setWebhook("https://sovervo98.scalingo.io/{}".format(secret), max_connections=1)
 
 app = Flask(__name__)
-"""users = []
-def update_data(interval):
-    print('CALLED update_data !!!!!!!!!!!!!!')
-    global users
-    print('USERS ARE : ', users)
-    for id in users:
-        print('TRY TO SEND msg to ', id)
-        bot.sendMessage(id, "each 10 sec")
-    Timer(interval, update_data, [interval]).start()
 
-# update data every second
-update_data(10)
-"""
+bot_token = "658605557:AAGQrDKPH3dqtPbDjvEK_I9BcW4VSr-A5yk"
 
-@app.route('/{}'.format(secret), methods=["POST"])
-def telegram_webhook():
-    print("request from telegram!!!!!")
-    update = request.get_json()
-    if "message" in update:
-        global users
-        text = update["message"]["text"]
-        print('MESSAGE: ', text)
-        chat_id = update["message"]["chat"]["id"]
-        if chat_id not in users:
-            users.append(chat_id)
-        bot.sendMessage(chat_id, "From the web: you said '{}'".format(text))
-    return "OK"
+def get_url(method):
+  return "https://api.telegram.org/bot{}/{}".format(bot_token,method)
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 24503))
-    app.run(host='0.0.0.0', port=port)
+def process_message(update):
+    data = {}
+    data["chat_id"] = update["message"]["from"]["id"]
+    data["text"] = "I can hear you!"
+    r = requests.post(get_url("sendMessage"), data=data)
+
+@app.route("/{}".format(bot_token), methods=["POST"])
+def process_update():
+    if request.method == "POST":
+        update = request.get_json()
+        if "message" in update:
+            process_message(update)
+        return "ok!", 200
 
