8154796c7cbc36c3f2aa45ea72829369ceb71f2c
Sova98 <marina.95.m@mail.ru>
2018-12-11 00:10:40 +0300

    yy

--- app.py
+++ app.py
@@ -1,28 +1,48 @@
 from flask import Flask, request
-import telebot
-from telebot import types
-import time
+import telepot
+import urllib3
+from threading import Timer
 
-secret = "n1hz7waCT9"
-bot = telebot.TeleBot('658605557:AAGQrDKPH3dqtPbDjvEK_I9BcW4VSr-A5yk', threaded=False)
+proxy_url = "http://proxy.server:3128"
+telepot.api._pools = {
+    'default': urllib3.ProxyManager(proxy_url=proxy_url, num_pools=3, maxsize=10, retries=False, timeout=30),
+}
+telepot.api._onetime_pool_spec = (urllib3.ProxyManager, dict(proxy_url=proxy_url, num_pools=1, maxsize=1, retries=False, timeout=30))
 
-bot.remove_webhook()
-time.sleep(1)
-bot.set_webhook(url="https://sovervo98.scalingo.io/{}".format(secret))
+secret = "bddad84a-f6a3-445b-8f1d-31f9e3b4697b"
+bot = telepot.Bot('658605557:AAGQrDKPH3dqtPbDjvEK_I9BcW4VSr-A5yk')
+bot.setWebhook("https://sova98.pythonanywhere.com/{}".format(secret), max_connections=1)
 
 app = Flask(__name__)
+"""users = []
+def update_data(interval, bot):
+    print('CALLED update_data !!!!!!!!!!!!!!')
+    global users
+    print('USERS ARE : ', users)
+    for id in users:
+        print('TRY TO SEND msg to ', id)
+        bot.sendMessage(id, "each 10 sec")
+    Timer(interval, update_data, [interval]).start()
+
+# update data every second
+update_data(10, bot)
+"""
 
 @app.route('/{}'.format(secret), methods=["POST"])
-def webhook():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    print("Message")
-    return "ok", 200
-
-
-@bot.message_handler(commands=['start', 'help'])
-def startCommand(message):
-    bot.send_message(message.chat.id, 'Hi *' + message.chat.first_name + '*!' , parse_mode='Markdown', reply_markup=types.ReplyKeyboardRemove())
+def telegram_webhook():
+    update = request.get_json()
+    print(update)
+    if "message" in update:
+        global users
+        text = update["message"]["text"]
+        chat_id = update["message"]["chat"]["id"]
+        if chat_id not in users:
+            users.append(chat_id)
+        bot.sendMessage(chat_id, "From the web: you said '{}'".format(text))
+    return "OK"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
+
