5c757324dbe3f956ced6fcd04d2e21561807100c
Sova98 <marina.95.m@mail.ru>
2018-12-10 23:44:26 +0300

    19th

--- app.py
+++ app.py
@@ -22,3 +22,8 @@ def process_update():
             process_message(update)
         return "ok!", 200
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
+
