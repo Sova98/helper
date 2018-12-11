534cd984c3eae7a5cf528a8b1eaa05bd8a099351
Sova98 <marina.95.m@mail.ru>
2018-12-11 10:53:43 +0300

    000

--- app.py
+++ app.py
@@ -1,34 +1,12 @@
-import os
-from flask import Flask
-from flask import render_template
-from flask import Flask, request
-import telepot
-import urllib3
-app = Flask(__name__)
-proxy_url = "https://128.199.154.45:8080"
-telepot.api._pools = {
-    'default': urllib3.ProxyManager(proxy_url=proxy_url, num_pools=3, maxsize=10, retries=False, timeout=30),
-}
-telepot.api._onetime_pool_spec = (urllib3.ProxyManager, dict(proxy_url=proxy_url, num_pools=1, maxsize=1, retries=False, timeout=30))
+# -*- coding: utf-8 -*-
+import config
+import telebot
+from telebot import apihelper
+bot = telebot.TeleBot(config.token)
+apihelper.proxy = {'https':'https://118.172.201.141:30104'}
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
+    bot.send_message(message.chat.id, message.text)
 
-secret = "bddad84a-f6a3-445b-8f1d-31f9e3b4697b"
-bot = telepot.Bot('658605557:AAGQrDKPH3dqtPbDjvEK_I9BcW4VSr-A5yk')
-bot.setWebhook("https://sovervo98.scalingo.io/{}".format(secret), max_connections=1)
-
-@app.route('/{}'.format(secret), methods=["POST"])
-def hello():
-    update = request.get_json()
-    print("updates", update)
-    if "message" in update:
-        global users
-        text = update["message"]["text"]
-        chat_id = update["message"]["chat"]["id"]
-        if chat_id not in users:
-            users.append(chat_id)
-        requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text=Hello%20World".format("658605557:AAGQrDKPH3dqtPbDjvEK_I9BcW4VSr-A5yk", chat_id))
-        bot.sendMessage(chat_id, "From the web: you said '{}'".format(text))
-    return "OK"
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+     bot.polling(none_stop=True)
---
+++ config.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+token="658605557:AAGQrDKPH3dqtPbDjvEK_I9BcW4VSr-A5yk"
